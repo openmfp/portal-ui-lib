86718415980c9c743d8b89a678be4b4b71b44316
Grzegorz Krajniak <gkrajniak@gmail.com>
2024-10-22 09:27:23 +0200

    feat: use refresh token for automatic session refresh

--- src/lib/models/auth.ts
+++ src/lib/models/auth.ts
@@ -3,7 +3,7 @@ export enum AuthEvent {
   AUTH_ERROR = 'AuthError',
   AUTH_EXPIRED = 'AuthExpired',
   AUTH_REFRESHED = 'AuthRefreshed',
-  LOGOUT = 'Logout',
   AUTH_EXPIRE_SOON = 'AuthExpireSoon',
   AUTH_CONFIG_ERROR = 'AuthConfigError',
+  LOGOUT = 'Logout',
 }
