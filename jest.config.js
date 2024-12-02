ba12a081952c728843374cc58f4eda5b3886967c
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2024-12-02 14:00:46 +0100

    feat: Improve error handling for config entity retrieval

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
       branches: 80,
       functions: 90,
       lines: 95,
-      statements: -34,
+      statements: -33,
     },
   },
   coveragePathIgnorePatterns: ['/node_modules/', '/integration-tests/'],
