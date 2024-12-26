cfc447701b4e94c0a6e64f5ac79729ebe0292df5
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2024-12-26 09:44:28 +0100

    feature: introduce web components approach and migrate error component

--- jest-global-mocks.ts
+++
@@ -1,7 +0,0 @@
-jest.mock('./projects/lib/src/lib/initializers', () => ({
-  ...jest.requireActual('./projects/lib/src/lib/initializers'),
-  provideBootstrap: jest.fn().mockReturnValue({
-    provide: 'PROVIDER',
-    useValue: {},
-  }),
-}));
--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   testRunner: 'jest-jasmine2',
-  moduleNameMapper: {
-    '^lodash-es(.*)': 'lodash',
-    '@openmfp/portal-ui-lib': '<rootDir>/projects/lib/src/public-api.ts',
-  },
   collectCoverage: true,
-  setupFilesAfterEnv: ['<rootDir>/jest-global-mocks.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 90,
-      lines: 95,
-      statements: -28,
-    },
-  },
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/dist-wc/'],
   coveragePathIgnorePatterns: ['/node_modules/', '/integration-tests/'],
 };
