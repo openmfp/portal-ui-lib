d78725d405f94cecd0c4bf0c817fe183b8fa12bc
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-04-16 15:36:03 +0200

    feat: Use @ui5/webcomponents-ngx for generic ui

--- projects/wc/src/app/initializers/luigi-wc-initializer.ts
+++ projects/wc/src/app/initializers/luigi-wc-initializer.ts
@@ -2,26 +2,31 @@ import { ErrorComponent } from '../components/error/error.component';
 import { DetailViewComponent } from '../components/generic-ui/detail-view/detail-view.component';
 import { ListViewComponent } from '../components/generic-ui/list-view/list-view.component';
 import { registerLuigiWebComponents } from '../utils/wc';
-import { APP_INITIALIZER, Injector, inject } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  EnvironmentProviders,
+  Injector,
+  inject,
+  makeEnvironmentProviders,
+} from '@angular/core';
 
-function initializeWC() {
-  const injector = inject(Injector);
-  registerLuigiWebComponents(
+export function provideLuigiWebComponents(): EnvironmentProviders {
+  return makeEnvironmentProviders([
     {
-      'error-component': ErrorComponent,
-      'generic-list-view': ListViewComponent,
-      'generic-detail-view': DetailViewComponent,
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        return () => {
+          registerLuigiWebComponents(
+            {
+              'error-component': ErrorComponent,
+              'generic-list-view': ListViewComponent,
+              'generic-detail-view': DetailViewComponent,
+            },
+            inject(Injector),
+          );
+        };
+      },
+      multi: true,
     },
-    injector,
-  );
-
-  return () => undefined;
+  ]);
 }
-
-const provider = {
-  provide: APP_INITIALIZER,
-  useFactory: initializeWC,
-  multi: true,
-};
-
-export const provideLuigiWebComponents = () => provider;
