ebe7f5a554d946c67324e03936c4a21cef6af6d7
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-04-25 16:27:13 +0200

    Add organization management feature with related UI components
    Introduced a new "organization management" feature, including relevant translations, a dedicated Angular component, and Luigi node configurations. Updated user profile service and providers to integrate the feature and ensure proper navigation support.

--- projects/lib/src/lib/services/luigi-config/user-profile-config.service.ts
+++ projects/lib/src/lib/services/luigi-config/user-profile-config.service.ts
@@ -1,14 +1,48 @@
+import { Injectable } from '@angular/core';
+
+export interface ExternalLink {
+  sameWindow: boolean;
+  URL: string;
+}
+
+export interface OpenNodeInModal {
+  title?: string;
+  size?: 's' | 'm' | 'l';
+  width?: string;
+  height?: string;
+}
+
 export interface UserProfileItem {
   label?: string;
   icon?: string;
   link?: string;
+  openNodeInModal?: boolean | OpenNodeInModal;
+  externalLink?: ExternalLink;
 }
 
 export interface UserProfile {
-  logout: UserProfileItem;
+  logout?: UserProfileItem;
   items: UserProfileItem[];
 }
 
 export interface UserProfileConfigService {
   getProfile(): Promise<UserProfile>;
+}
+
+@Injectable({ providedIn: 'root' })
+export class UserProfileConfigServiceImpl implements UserProfileConfigService {
+  async getProfile(): Promise<UserProfile> {
+    return {
+      items: [
+        {
+          label: 'PROFILE_ORGANIZATION',
+          icon: 'building',
+          link: '/organization-management',
+          openNodeInModal: {
+            size: 'm',
+          },
+        },
+      ],
+    };
+  }
 }
---
+++ projects/wc/src/app/components/organization-management/organization-management.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'wc-organization-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './organization-management.component.html',
+  styleUrl: './organization-management.component.scss',
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class OrganizationManagementComponent {}
