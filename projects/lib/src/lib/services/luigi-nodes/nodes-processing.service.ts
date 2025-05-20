9e05b28e1863f987b95e38d034bf989c4853e9af
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-05-20 15:37:45 +0200

    Refactor node processing and introduce helper function
    Extracted logic for handling `defineEntity` nodes into a new helper method `processNodeDefineEntity`. This improves readability and simplifies main logic flow in `nodes-processing.service.ts`. Additional cleanup and adjustments were made to streamline child node handling and recursive processing.

--- projects/lib/src/lib/services/luigi-nodes/nodes-processing.service.ts
+++ projects/lib/src/lib/services/luigi-nodes/nodes-processing.service.ts
@@ -63,32 +63,20 @@ export class NodesProcessingService {
       node._portalDirectChildren = node.children;
     }
     const directChildren = node._portalDirectChildren || [];
-    let newEntityPath = parentEntityPath;
 
     if (node.defineEntity) {
-      if (parentEntityPath?.length > 0) {
-        newEntityPath = parentEntityPath + '.' + node.defineEntity.id;
-      } else {
-        newEntityPath = node.defineEntity.id;
-      }
-
-      node.children = (ctx: any) => {
-        return this.entityChildrenProvider(
-          node,
-          ctx,
-          childrenByEntity,
-          directChildren,
-          newEntityPath,
-        );
-      };
-
-      this.processCompoundNode(node, childrenByEntity, newEntityPath);
+      this.processNodeDefineEntity(
+        node,
+        childrenByEntity,
+        parentEntityPath,
+        directChildren,
+      );
     } else {
       directChildren.forEach((child) => {
         this.applyEntityChildrenRecursively(
           child,
           childrenByEntity,
-          newEntityPath,
+          parentEntityPath,
         );
       });
       node.children = async (ctx: any) =>
@@ -108,6 +96,32 @@ export class NodesProcessingService {
     if (node.virtualTree) {
       node.children = undefined;
     }
+  }
+
+  private processNodeDefineEntity(
+    node: LuigiNode,
+    childrenByEntity: Record<string, LuigiNode[]>,
+    parentEntityPath: string,
+    directChildren: LuigiNode[],
+  ) {
+    let newEntityPath = parentEntityPath;
+    if (parentEntityPath?.length > 0) {
+      newEntityPath = parentEntityPath + '.' + node.defineEntity.id;
+    } else {
+      newEntityPath = node.defineEntity.id;
+    }
+
+    node.children = (ctx: any) => {
+      return this.entityChildrenProvider(
+        node,
+        ctx,
+        childrenByEntity,
+        directChildren,
+        newEntityPath,
+      );
+    };
+
+    this.processCompoundNode(node, childrenByEntity, newEntityPath);
   }
 
   processCompoundNode(
@@ -151,60 +165,20 @@ export class NodesProcessingService {
     directChildren?: LuigiNode[],
     entityPath?: string,
   ) {
-    const createChildrenList = (
-      children: LuigiNode[],
-      staticChildren?: LuigiNode[],
-    ) => {
-      const entityRootChildren = staticChildren ? [] : children;
-      let mergedChildrenByEntity = childrenByEntity;
-      if (staticChildren) {
-        const entityChildrenByEntity: Record<string, LuigiNode[]> = {};
-
-        children?.forEach((child) => {
-          if (
-            child.entityType === entityPath ||
-            child.entityType === EntityType.ENTITY_ERROR ||
-            staticChildren.includes(child)
-          ) {
-            entityRootChildren.push(child);
-          } else if (child.entityType) {
-            if (!entityChildrenByEntity[child.entityType]) {
-              entityChildrenByEntity[child.entityType] = [];
-            }
-            entityChildrenByEntity[child.entityType].push(child);
-          } else {
-            console.warn('Ignored entity child, no entity type defined', child);
-          }
-        });
-        mergedChildrenByEntity = { ...childrenByEntity };
-        Object.keys(entityChildrenByEntity).forEach((key) => {
-          const existingNodes = mergedChildrenByEntity[key];
-          mergedChildrenByEntity[key] = existingNodes
-            ? [...existingNodes, ...entityChildrenByEntity[key]]
-            : entityChildrenByEntity[key];
-        });
-      }
-
-      entityRootChildren.forEach((child) => {
-        this.applyEntityChildrenRecursively(
-          child,
-          mergedChildrenByEntity,
-          entityPath,
-        );
-      });
-      return this.childrenNodesService.processChildrenForEntity(
-        entityNode,
-        entityRootChildren,
-        ctx,
-      );
-    };
-
     return new Promise<LuigiNode[]>(async (resolve, reject) => {
       const entityTypeId = entityPath || entityNode?.defineEntity?.id;
       const entityIdContextKey = entityNode?.defineEntity?.contextKey;
       if (!entityTypeId) {
         console.warn('No entity node!'); //TODO: check if needed or assured before
-        resolve(createChildrenList(directChildren));
+        resolve(
+          this.createChildrenList(
+            entityNode,
+            ctx,
+            childrenByEntity,
+            directChildren,
+            entityPath,
+          ),
+        );
       } else {
         const entityId = ctx[entityIdContextKey];
         const staticChildren = [
@@ -223,13 +197,36 @@ export class NodesProcessingService {
               fetchContext.get(dynamicFetchId),
             )
             .then((children) => {
-              resolve(createChildrenList(children, staticChildren));
+              resolve(
+                this.createChildrenList(
+                  entityNode,
+                  ctx,
+                  childrenByEntity,
+                  children,
+                  entityPath,
+                  staticChildren,
+                ),
+              );
             })
             .catch((error) => {
-              resolve(createChildrenList(staticChildren));
+              resolve(
+                this.createChildrenList(
+                  entityNode,
+                  ctx,
+                  childrenByEntity,
+                  staticChildren,
+                  entityPath,
+                ),
+              );
             });
         } else {
-          const childrenList = await createChildrenList(staticChildren);
+          const childrenList = await this.createChildrenList(
+            entityNode,
+            ctx,
+            childrenByEntity,
+            staticChildren,
+            entityPath,
+          );
           console.debug(`children list ${childrenList.length}`);
           resolve(
             this.luigiNodesService.replaceServerNodesWithLocalOnes(
@@ -240,5 +237,57 @@ export class NodesProcessingService {
         }
       }
     });
+  }
+
+  private createChildrenList(
+    entityNode: LuigiNode,
+    ctx: any,
+    childrenByEntity: Record<string, LuigiNode[]>,
+    children: LuigiNode[],
+    entityPath?: string,
+    staticChildren?: LuigiNode[],
+  ) {
+    const entityRootChildren = staticChildren ? [] : children;
+    let mergedChildrenByEntity = childrenByEntity;
+    if (staticChildren) {
+      const entityChildrenByEntity: Record<string, LuigiNode[]> = {};
+
+      children?.forEach((child) => {
+        if (
+          child.entityType === entityPath ||
+          child.entityType === EntityType.ENTITY_ERROR ||
+          staticChildren.includes(child)
+        ) {
+          entityRootChildren.push(child);
+        } else if (child.entityType) {
+          if (!entityChildrenByEntity[child.entityType]) {
+            entityChildrenByEntity[child.entityType] = [];
+          }
+          entityChildrenByEntity[child.entityType].push(child);
+        } else {
+          console.warn('Ignored entity child, no entity type defined', child);
+        }
+      });
+      mergedChildrenByEntity = { ...childrenByEntity };
+      Object.keys(entityChildrenByEntity).forEach((key) => {
+        const existingNodes = mergedChildrenByEntity[key];
+        mergedChildrenByEntity[key] = existingNodes
+          ? [...existingNodes, ...entityChildrenByEntity[key]]
+          : entityChildrenByEntity[key];
+      });
+    }
+
+    entityRootChildren.forEach((child) => {
+      this.applyEntityChildrenRecursively(
+        child,
+        mergedChildrenByEntity,
+        entityPath,
+      );
+    });
+    return this.childrenNodesService.processChildrenForEntity(
+      entityNode,
+      entityRootChildren,
+      ctx,
+    );
   }
 }
