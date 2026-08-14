db93828cf3822bd0c7a1d6910aad76212c92ad75
gkrajniak <gkrajniak@gmail.com>
2026-08-14 10:36:19 +0200

    Fix linting
    Signed-off-by: gkrajniak <gkrajniak@gmail.com>

--- projects/wc/src/main.ts
+++ projects/wc/src/main.ts
@@ -1,5 +1,7 @@
-import '@angular/localize/init';
 import { appConfig } from './app/app.config';
+import '@angular/localize/init';
 import { createApplication } from '@angular/platform-browser';
 
-createApplication(appConfig).catch((err) => console.error(err));
+createApplication(appConfig).catch((err) => {
+  console.error(err);
+});
